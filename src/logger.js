88ffd5f066d20e00bcf8a12098eb3ff96414a8a2
Adrien M <adrien@d.tube>
2018-10-05 02:10:33 +0200

    add file logger

--- src/logger.js
+++ src/logger.js
@@ -5,12 +5,23 @@ log4js.configure({
     out: { type: 'stdout', layout: {
       type: 'pattern',
       pattern: '%[%d{hh:mm:ss.SSS} [%p]%] %m',
-    }}
+    }},
+    file: {
+      type: 'file',
+      filename: './logs/output.log',
+      maxLogSize: 10485760,
+      backups: 3,
+      compress: true
+    }
   },
-  categories: { default: { appenders: ['out'], level: 'info' } }
+  categories: { 
+    default: { 
+      appenders: ['out', 'file'],
+      level: process.env.LOG_LEVEL || 'info'
+    }
+  }
 })
 
 var logger = log4js.getLogger();
-logger.level = process.env.LOG_LEVEL || 'info';
 logger.info("Logger initialized");
 module.exports = logger
