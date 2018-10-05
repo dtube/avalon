393094d4a653d4969b9a712ca1efbbfbe1620dd5
heimindanger <adrien@d.tube>
2018-10-05 02:03:24 +0200

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-#HTTP_PORT=3001
-#P2P_PORT=6001
-#DB_NAME=avalon
-#DB_URL=mongodb://localhost:27017
-#PEERS=
-export NODE_OWNER=master
-export NODE_OWNER_PUB=dTuBhkU6SUx9JEx1f4YEt34X9sC7QGso2dSrqE8eJyfz
-export NODE_OWNER_PRIV=34EpMEDFJwKbxaF7FhhLyEe3AhpM4dwHMLVfs4JyRto5
+#export HTTP_PORT=3001
+#export P2P_PORT=6001
+#export DB_NAME=avalon
+#export DB_URL=mongodb://localhost:27017
+export LOG_LEVEL=debug
+export PEERS=ws://api.avalon.wtf:6001,ws://avalon.nannal.com:6001,ws://82.66.109.22:6001
+export NODE_OWNER=<your_username>
+export NODE_OWNER_PUB=<your_public_key>
+export NODE_OWNER_PRIV=<your_private_key>
 npm start
