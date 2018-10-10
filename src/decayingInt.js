a010dd4ad047b89304440380a91b044240a766dc
Adrien M <adrien@d.tube>
2018-10-11 01:01:29 +0200

    add exponential decaying variable type

---
+++ src/decayingInt.js
@@ -0,0 +1,35 @@
+// this class simplifies managing variables that are depleting over time
+// raw should be of the format {v:<int>,t:<timestamp>}
+// config must have: halflife
+// config can also have: max.
+
+module.exports = class DecayingInt {
+    constructor(raw, config) {
+        if (!config.max)
+            config.max = Number.MAX_SAFE_INTEGER
+        config.lifetime = Math.log(2)/config.halflife
+        this.v = raw.v
+        this.t = raw.t
+        this.config = config
+    }
+
+    decay(time) {
+        if (time < this.t) return
+
+        var tmpValue = this.v
+        var timeDiff = time-this.t
+        tmpValue = tmpValue*Math.exp(-this.config.lifetime*timeDiff)
+        
+        var newValue = Math.ceil(tmpValue)
+        var newTime = Math.ceil(this.t-Math.log(newValue/this.v)/this.config.lifetime)
+
+        if (newValue > this.config.max)
+            newValue = this.config.max
+
+        return {
+            v: newValue,
+            t: newTime
+        }
+    }
+}
+
