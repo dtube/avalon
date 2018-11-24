34d5b310618fdf870b27da6bfd02df9c344dceba
heimindanger <adrien@d.tube>
2018-11-24 21:10:31 +0100

    Delete spam_transfer.sh

--- scripts/spam_transfer.sh
+++
@@ -1,4 +0,0 @@
-curl -H "Content-type:application/json" --data $(node src/cli.js sign 34EpMEDFJwKbxaF7FhhLyEe3AhpM4dwHMLVfs4JyRto5 master '{"type":3,"data":{"receiver":"miner1", "amount":1000}}') http://localhost:3001/transact
-sleep 1
-curl -H "Content-type:application/json" --data $(node src/cli.js sign 8cxx3Ly7xkDAghFnZRqM8Wi1xhwM8CBCkaAPL6NMgCRS miner1 '{"type":3,"data":{"receiver":"master", "amount":999}}') http://localhost:3001/transact
-sleep 1
