1c24ebff9a3af7c72b6328df20fdd082a7514088
Adrien M <adrien@d.tube>
2018-11-28 00:17:27 +0100

    add pagination to /blog, /feed and /new

--- src/http.js
+++ src/http.js
@@ -122,6 +122,21 @@ var http = {
                 res.send(contents)
             })
         })
+        app.get('/new/:author/:link', (req, res) => {
+            db.collection('contents').findOne({
+            $and: [
+                {author: req.params.author}, 
+                {link: req.params.link}
+            ]}, function(err, content) {
+                db.collection('contents').find({
+                $and: [
+                    {pa: null},
+                    {_id: {$lt: content._id}}
+                ]}, {sort: {_id: -1}}).toArray(function(err, contents) {
+                    res.send(contents)
+                })
+            })
+        })
 
         // get feed contents
         app.get('/feed/:username', (req, res) => {
@@ -134,14 +149,48 @@ var http = {
                     res.send(contents)
                 })
             })
-            
+        })
+        app.get('/feed/:username/:author/:link', (req, res) => {
+            db.collection('contents').findOne({
+            $and: [
+                {author: req.params.author}, 
+                {link: req.params.link}
+            ]}, function(err, content) {
+                db.collection('accounts').findOne({name: req.params.username}, function(err, account) {
+                    db.collection('contents').find({
+                    $and: [
+                        {author: {$in: account.follows}},
+                        {pa: null},
+                        {_id: {$lt: content._id}}
+                    ]}, {sort: {_id: -1}}).toArray(function(err, contents) {
+                        res.send(contents)
+                    })
+                })
+            })
         })
 
         // get blog of user
-        app.get('/blog/:author', (req, res) => {
-            var author = req.params.author
-            db.collection('contents').find({pa: null, author: author}, {sort: {_id: -1}}).toArray(function(err, contents) {
+        app.get('/blog/:username', (req, res) => {
+            var username = req.params.username
+            db.collection('contents').find({pa: null, author: username}, {sort: {_id: -1}}).toArray(function(err, contents) {
                 res.send(contents)
+            })
+        })
+        app.get('/blog/:username/:author/:link', (req, res) => {
+            db.collection('contents').findOne({
+            $and: [
+                {author: req.params.author}, 
+                {link: req.params.link}
+            ]}, function(err, content) {
+                var username = req.params.username
+                db.collection('contents').find({
+                $and: [
+                    {pa: null},
+                    {author: username},
+                    {_id: {$lt: content._id}}
+                ]}, {sort: {_id: -1}}).toArray(function(err, contents) {
+                    res.send(contents)
+                })
             })
         })
 
