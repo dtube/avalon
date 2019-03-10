e00cd75fb7f5c79f078d9ae404a4d210dd210972
nannal <d.abbott@nannal.com>
2019-03-10 13:42:36 +0200

    Arbitary Test Network Size

--- scripts/test_network.sh
+++ scripts/test_network.sh
@@ -1,27 +1,90 @@
 #!/bin/bash
+
+#Defaults
+acc_Req=5
+acc_Made=0
+acc_Def_Name="miner"
 PRIVATE_KEY=34EpMEDFJwKbxaF7FhhLyEe3AhpM4dwHMLVfs4JyRto5
 
-# create accounts
-node src/cli.js createAccount $PRIVATE_KEY master sXHqzsMDENCVUCXuY8KL52afpFWzeeVRaPaD44rV5ZQX miner1
-node src/cli.js createAccount $PRIVATE_KEY master tWWLqc5wPTbXPaWrFAfqUwGtEBLmUbyavp3utwPUop2g miner2
-node src/cli.js createAccount $PRIVATE_KEY master d2EdJPNgFBwd1y9vhMzxw6vELRneC1gSHVEjguTG74Ce miner3
+#Array Declaration
+acc_Name=()
+acc_Pub=()
+acc_Priv=()
 
-sleep 6
+#Parse flags
+while test $# -gt 0; do
+        case "$1" in
+                -h|--help)
+                        echo "options:"
+                        echo "-h, --help                Show this message"
+                        echo "-a,      specify the number of accounts to make"
+                        echo "-n,      name of the accounts"
+                        exit 0
+                        ;;
+                -a)
+                        shift
+                        if test $# -gt 0; then
+                                export acc_Req=$1
+                        else
+                                echo "No accounts requested"
+                                exit 1
+                        fi
+                        shift
+                        ;;
+                -n)
+                        shift
+                        if test $# -gt 0; then
+                                export acc_Def_Name=$1
+                        else
+                                echo "No accounts requested"
+                                exit 1
+                        fi
+                        shift
+                        ;;
+        esac
+done
+
+
+while [ $acc_Made != $acc_Req ]
+do
+  #We need to do this to make JQ happy
+  cmd=$(node ./src/cli.js keypair|sed s/\ pub:/\"pub\":/g|sed s/priv:/\"priv\":/g|sed s/' }'/'}'/g|tr -d '\n'|tr -d ' '|sed s/\'/\"/g)
+
+
+  acc_Made=$(($acc_Made+1))
+  acc_Pub+=($(echo $cmd|jq -r .pub))
+  acc_Priv+=($(echo $cmd|jq -r .priv))
+  acc_Name+=($(echo $acc_Def_Name$acc_Made))
+done
+echo ${acc_Name[@]}
+
+# create accounts
+name=0
+for key in "${acc_Pub[@]}"
+do node src/cli.js createAccount $PRIVATE_KEY master $key ${acc_Name[name]}
+name=$name+1
+done
+
+
+sleep 5
 
 # send some tokens
-node src/cli.js transfer $PRIVATE_KEY master miner1 10000
-node src/cli.js transfer $PRIVATE_KEY master miner2 10000
-node src/cli.js transfer $PRIVATE_KEY master miner3 10000
+for name in "${acc_Name[@]}"
+do node src/cli.js transfer $PRIVATE_KEY master $name 10000
+done
 
-sleep 15
+sleep 10
 
 # display nodes ip and port in profile
 node src/cli.js profile $PRIVATE_KEY master '{"node":{"ws":"ws://127.0.0.1:6001"}}'
-node src/cli.js profile 8cxx3Ly7xkDAghFnZRqM8Wi1xhwM8CBCkaAPL6NMgCRS miner1 '{"node":{"ws":"ws://127.0.0.1:6002"}}'
-node src/cli.js profile GhjBE9hvQcLhBicFdSB1ZmSnLmAN3vD2mjjNoQKxEuSc miner2 '{"node":{"ws":"ws://127.0.0.1:6003"}}'
-node src/cli.js profile 7vMKdxKCYTyzayaeiDirTgRhPwnFumzgh2TebT58EFKw miner3 '{"node":{"ws":"ws://127.0.0.1:6004"}}'
-
+name=0
+for key in "${acc_Priv[@]}"
+do port=$name+6001
+node src/cli.js profile $key ${acc_Name[name]} '{"node":{"ws":"ws://127.0.0.1:'$port'"}}'
+name=$name+1
+done
 # everyone votes for itself
-node src/cli.js approveNode 8cxx3Ly7xkDAghFnZRqM8Wi1xhwM8CBCkaAPL6NMgCRS miner1 miner1
-node src/cli.js approveNode GhjBE9hvQcLhBicFdSB1ZmSnLmAN3vD2mjjNoQKxEuSc miner2 miner2
-node src/cli.js approveNode 7vMKdxKCYTyzayaeiDirTgRhPwnFumzgh2TebT58EFKw miner3 miner3
+name=0
+for key in "${acc_Priv[@]}"
+do node src/cli.js approveNode $key ${acc_Name[name]} ${acc_Name[name]}
+done
