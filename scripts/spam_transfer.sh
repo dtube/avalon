c67231d62f1a30377014f94a83c40484507567f3
nannal <dabbott@localhost.localdomain>
2019-03-14 22:02:47 +0200

    Changes to spam transfer to add in sleep and make it work

--- scripts/spam_transfer.sh
+++ scripts/spam_transfer.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
-if [ $1="" ]
-then echo "./spam_transfer.sh account1 priv1 account2 priv2"
+if [ -z $1 ]
+then echo "./spam_transfer.sh account1 priv1 account2 priv2 sleep"
+echo $1
 exit
 fi
-$1=acc1
-$2=priv1
-$3=acc2
-$4=priv2
+acc1=$1
+priv1=$2
+acc2=$3
+priv2=$4
+sleep=$5
 
 while true
 do node ./src/cli.js transfer $priv1 $acc1 $acc2 1 &
 node ./src/cli.js transfer $priv2 $acc2 $acc1 1 &
+sleep $sleep
 done
