42496699e91712e6d181e8dff3c09efbd80a382e
Adrien M <adrien@d.tube>
2019-04-07 02:22:36 +0200

    order cli commands alphabetically

--- src/cli.js
+++ src/cli.js
@@ -17,43 +17,8 @@ program
     .option('-M, --me [my_username]', 'username of the transactor')
     .option('-A, --api [api_url]', 'avalon api url')
     .option('-S, --spam [delay_in_ms]', 'repeats the tx every delay')
-    
-program
-    .command('keypair')
-    .description('generate a new keypair')
-    .alias('key')
-    .option('-P, --prefix [prefix]', 'public key prefix')
-    .action(function(options) {
-        var prefix = (options.prefix || '')
-        var priv
-        var pub
-        var pub58
-        do {
-            priv = randomBytes(config.randomBytesLength)
-            pub = secp256k1.publicKeyCreate(priv)
-            pub58 = bs58.encode(pub)
-        } while (!pub58.startsWith(prefix) || !secp256k1.privateKeyVerify(priv))
 
-        writeLine(JSON.stringify({
-            pub: pub58,
-            priv: bs58.encode(priv)
-        }))
-    })
-
-program
-    .command('sign <transaction>')
-    .description('sign a tx w/o broadcasting')
-    .action(function(transaction) {
-        verifyKeyAndUser()
-        writeLine(JSON.stringify(cmds.sign(program.key, program.me, transaction)))
-    }).on('--help', function(){
-        writeLine('')
-        writeLine('Example:')
-        writeLine('  $ sign \'{"type":1,"data":{"target":"bob"}}\' -F key.json -M alice')
-    })
-
-program
-    .command('account <pub_key> <new_user>')
+program.command('account <pub_key> <new_user>')
     .description('create a new account')
     .action(function(pubKey, newUser) {
         verifyKeyAndUser()
@@ -69,45 +34,7 @@ program
         writeLine('  $ account fR3e4CcvMRuv8yaGtoQ6t6j1hxfyocqhsKHi2qP9mb1E fr3e4ccvmruv8yagtoq6t6j1hxfyocqhskhi2qp9mb1e -F key.json -M alice')
     })
 
-program
-    .command('vote-leader <leader>')
-    .description('vote for a leader')
-    .action(function(leader) {
-        verifyKeyAndUser()
-        sendTx(cmds.approveNode(program.key, program.me, leader))
-    }).on('--help', function(){
-        writeLine('')
-        writeLine('Example:')
-        writeLine('  $ vote-leader bob -F key.json -M alice')
-    })
-
-program
-    .command('unvote-leader <leader>')
-    .description('remove a leader vote')
-    .action(function(leader) {
-        verifyKeyAndUser()
-        sendTx(cmds.disapproveNode(program.key, program.me, leader))
-    }).on('--help', function(){
-        writeLine('')
-        writeLine('Example:')
-        writeLine('  $ unvote-leader bob -F key.json -M alice')
-    })
-
-program
-    .command('transfer <receiver> <amount>')
-    .alias('xfer')
-    .description('transfer coins')
-    .action(function(receiver, amount) {
-        verifyKeyAndUser()
-        sendTx(cmds.transfer(program.key, program.me, receiver, amount))
-    }).on('--help', function(){
-        writeLine('')
-        writeLine('Example:')
-        writeLine('  $ transfer bob 777 -F key.json -M alice')
-    })
-
-program
-    .command('comment <link> <pa> <pp> <json> <vt> <tag>')
+program.command('comment <link> <pa> <pp> <json> <vt> <tag>')
     .description('publish a new JSON content')
     .action(function(link, pa, pp, json, vt, tag) {
         verifyKeyAndUser()
@@ -127,63 +54,7 @@ program
         writeLine('  $ comment reply-to-bob bobs-post bob \'{"body": "Hello Bob"}\' 1 my-tag -F key.json -M alice')
     })
 
-program
-    .command('vote <link> <author> <vt> <tag>')
-    .description('publish a new JSON content')
-    .action(function(link, author, vt, tag) {
-        verifyKeyAndUser()
-        sendTx(cmds.vote(program.key, program.me, link, author, vt, tag))
-    }).on('--help', function(){
-        writeLine('')
-        writeLine('Arguments:')
-        writeLine('  <link>: the identifier of the comment to vote on')        
-        writeLine('  <author>: the author of the comment to vote on')
-        writeLine('  <vt>: the amount of VT to spend on the vote')
-        writeLine('  <tag>: the tag to associate with the vote')
-        writeLine('')
-        writeLine('Examples:')
-        writeLine('  $ vote root-comment alice 1000 introduce-yourself -F key.json -M bob')
-    })
-
-program
-    .command('promote <link> <pa> <pp> <json> <vt> <tag> <burn>')
-    .description('publish and promote')
-    .action(function(link, pa, pp, json, vt, tag, burn) {
-        verifyKeyAndUser()
-        sendTx(cmds.promotedComment(program.key, program.me, link, pa, pp, json, vt, tag, burn))
-    }).on('--help', function(){
-        writeLine('')
-        writeLine('Arguments:')
-        writeLine('  <link>: an arbitrary string identifying your content')        
-        writeLine('  <pa>: parent author (if you are replying to another comment)')
-        writeLine('  <pp>: parent link (if you are replying to another comment)')
-        writeLine('  <json>: a json object')
-        writeLine('  <vt>: the amount of VT to spend on the forced vote')
-        writeLine('  <tag>: the tag of the forced vote')
-        writeLine('  <burn>: the amount of coins to burn for promotion')
-        writeLine('')
-        writeLine('WARNING:')
-        writeLine('  Your balance will be reduced by <burn> coins')
-        writeLine('')
-        writeLine('Example:')
-        writeLine('  $ promote big-video \'\' \'\' \'{"title": "Check this out"}\' 777 my-tag 10 -F key.json -M alice')
-    })
-
-program
-    .command('profile <json>')
-    .alias('userJson')
-    .description('modify an account profile')
-    .action(function(json) {
-        verifyKeyAndUser()
-        sendTx(cmds.profile(program.key, program.me, json))
-    }).on('--help', function(){
-        writeLine('')
-        writeLine('Example:')
-        writeLine('  $ profile \'{"profile":{"avatar":"https://i.imgur.com/4Bx2eQt.jpg"}}\' -F key.json -M bob')
-    })
-
-program
-    .command('follow <target>')
+program.command('follow <target>')
     .alias('subscribe')
     .description('start following another user')
     .action(function(target) {
@@ -195,94 +66,38 @@ program
         writeLine('  $ follow bob -F key.json -M alice')
     })
 
-program
-    .command('unfollow <target>')
-    .alias('unsubscribe')
-    .description('stop following another user ')
-    .action(function(target) {
-        verifyKeyAndUser()
-        sendTx(cmds.unfollow(program.key, program.me, target))
-    }).on('--help', function(){
-        writeLine('')
-        writeLine('Example:')
-        writeLine('  $ unfollow bob -F key.json -M alice')
-    })
+program.command('keypair')
+    .description('generate a new keypair')
+    .alias('key')
+    .option('-H, --has [text]', 'generated public key will include the specified text')
+    .action(function(options) {
+        var has = (options.has || '')
+        has = has.toLowerCase()
+        var priv
+        var pub
+        var pub58
+        do {
+            priv = randomBytes(config.randomBytesLength)
+            pub = secp256k1.publicKeyCreate(priv)
+            pub58 = bs58.encode(pub)
+        } while ((pub58.toLowerCase().indexOf(has) === -1) 
+            || !secp256k1.privateKeyVerify(priv))
 
-program
-    .command('new-key <id> <pub> <allowed_txs>')
-    .description('add new key with custom perms')
-    .action(function(id, pub, allowedTxs) {
-        verifyKeyAndUser()
-        sendTx(cmds.newKey(program.key, program.me, id, pub, allowedTxs))
+        writeLine(JSON.stringify({
+            pub: pub58,
+            priv: bs58.encode(priv)
+        }))
     }).on('--help', function(){
         writeLine('')
-        writeLine('Transaction Types:')
-        for (const key in TransactionType)
-            writeLine('  '+TransactionType[key]+': '+key)
+        writeLine('Tip:')
+        writeLine('  You can save your keys to a file with the \'>\' operator')
         writeLine('')
         writeLine('Examples:')
-        writeLine('  $ new-key posting tWWLqc5wPTbXPaWrFAfqUwGtEBLmUbyavp3utwPUop2g [4,5,6,7,8] -F key.json -M alice')
-        writeLine('  $ new-key finance wyPSnqfmAKoz5gAWyPcND7Rot6es2aFgcDGDTYB89b4q [3] -F key.json -M alice')
+        writeLine('  $ keypair --has 2pac')
+        writeLine('  $ keypair > secret.json')
     })
 
-program
-    .command('remove-key <id>')
-    .description('remove a previously added key')
-    .action(function(id) {
-        verifyKeyAndUser()
-        sendTx(cmds.removeKey(program.key, program.me, id))
-    }).on('--help', function(){
-        writeLine('')
-        writeLine('Example:')
-        writeLine('  $ remove-key posting -F key.json -M alice')
-    })
-
-program
-    .command('password <pub>')
-    .description('change your master key')
-    .action(function(pub) {
-        verifyKeyAndUser()
-        sendTx(cmds.changePassword(program.key, program.me, pub))
-    }).on('--help', function(){
-        writeLine('')
-        writeLine('Arguments:')
-        writeLine('  <pub>: the new public key that will have full control over your account')
-        writeLine('')
-        writeLine('WARNING:')
-        writeLine('  DO NOT lose the new associated private key!')
-        writeLine('')
-        writeLine('Example:')
-        writeLine('  $ change-password tK9DqTygrcwGWZPsyVtZXNpfiZcAZN83nietKbKY8aiH -F key.json -M alice')
-    })
-
-program
-    .command('transfer-vt <receiver> <amount>')
-    .alias('xfer-vt')
-    .description('transfer voting tokens')
-    .action(function(receiver, amount) {
-        verifyKeyAndUser()
-        sendTx(cmds.transferVt(program.key, program.me, receiver, amount))
-    }).on('--help', function(){
-        writeLine('')
-        writeLine('Example:')
-        writeLine('  $ xfer-vt charlotte 777 -F key.json -M alice')
-    })
-
-program
-    .command('transfer-bw <receiver> <amount>')
-    .alias('xfer-bw')
-    .description('transfer bandwidth')
-    .action(function(receiver, amount) {
-        verifyKeyAndUser()
-        sendTx(cmds.transferBw(program.key, program.me, receiver, amount))
-    }).on('--help', function(){
-        writeLine('')
-        writeLine('Example:')
-        writeLine('  $ xfer-bw dan 777 -F key.json -M alice')
-    })
-
-program
-    .command('license')
+program.command('license')
     .description('read the software license')
     .action(function() {
         writeLine('Copyright (c) 2018 Adrien Marie. https://d.tube')
@@ -306,7 +121,199 @@ program
         writeLine('THE SOFTWARE.')
     })
 
-// error on unknown commands
+program.command('new-key <id> <pub> <allowed_txs>')
+    .description('add new key with custom perms')
+    .action(function(id, pub, allowedTxs) {
+        verifyKeyAndUser()
+        sendTx(cmds.newKey(program.key, program.me, id, pub, allowedTxs))
+    }).on('--help', function(){
+        writeLine('')
+        writeLine('Transaction Types:')
+        for (const key in TransactionType)
+            writeLine('  '+TransactionType[key]+': '+key)
+        writeLine('')
+        writeLine('Examples:')
+        writeLine('  $ new-key posting tWWLqc5wPTbXPaWrFAfqUwGtEBLmUbyavp3utwPUop2g [4,5,6,7,8] -F key.json -M alice')
+        writeLine('  $ new-key finance wyPSnqfmAKoz5gAWyPcND7Rot6es2aFgcDGDTYB89b4q [3] -F key.json -M alice')
+    })
+
+program.command('password <pub>')
+    .description('change your master key')
+    .action(function(pub) {
+        verifyKeyAndUser()
+        sendTx(cmds.changePassword(program.key, program.me, pub))
+    }).on('--help', function(){
+        writeLine('')
+        writeLine('Arguments:')
+        writeLine('  <pub>: the new public key that will have full control over your account')
+        writeLine('')
+        writeLine('WARNING:')
+        writeLine('  DO NOT lose the new associated private key!')
+        writeLine('')
+        writeLine('Example:')
+        writeLine('  $ change-password tK9DqTygrcwGWZPsyVtZXNpfiZcAZN83nietKbKY8aiH -F key.json -M alice')
+    })
+
+program.command('profile <json>')
+    .alias('user-json')
+    .description('modify an account profile')
+    .action(function(json) {
+        verifyKeyAndUser()
+        sendTx(cmds.profile(program.key, program.me, json))
+    }).on('--help', function(){
+        writeLine('')
+        writeLine('Example:')
+        writeLine('  $ profile \'{"profile":{"avatar":"https://i.imgur.com/4Bx2eQt.jpg"}}\' -F key.json -M bob')
+    })
+
+program.command('promote <link> <pa> <pp> <json> <vt> <tag> <burn>')
+    .description('publish and promote')
+    .action(function(link, pa, pp, json, vt, tag, burn) {
+        verifyKeyAndUser()
+        sendTx(cmds.promotedComment(program.key, program.me, link, pa, pp, json, vt, tag, burn))
+    }).on('--help', function(){
+        writeLine('')
+        writeLine('Arguments:')
+        writeLine('  <link>: an arbitrary string identifying your content')        
+        writeLine('  <pa>: parent author (if you are replying to another comment)')
+        writeLine('  <pp>: parent link (if you are replying to another comment)')
+        writeLine('  <json>: a json object')
+        writeLine('  <vt>: the amount of VT to spend on the forced vote')
+        writeLine('  <tag>: the tag of the forced vote')
+        writeLine('  <burn>: the amount of coins to burn for promotion')
+        writeLine('')
+        writeLine('WARNING:')
+        writeLine('  Your balance will be reduced by <burn> coins')
+        writeLine('')
+        writeLine('Example:')
+        writeLine('  $ promote big-video \'\' \'\' \'{"title": "Check this out"}\' 777 my-tag 10 -F key.json -M alice')
+    })
+
+program.command('remove-key <id>')
+    .description('remove a previously added key')
+    .action(function(id) {
+        verifyKeyAndUser()
+        sendTx(cmds.removeKey(program.key, program.me, id))
+    }).on('--help', function(){
+        writeLine('')
+        writeLine('Example:')
+        writeLine('  $ remove-key posting -F key.json -M alice')
+    })
+
+program.command('sign <transaction>')
+    .description('sign a tx w/o broadcasting')
+    .action(function(transaction) {
+        verifyKeyAndUser()
+        writeLine(JSON.stringify(cmds.sign(program.key, program.me, transaction)))
+    }).on('--help', function(){
+        writeLine('')
+        writeLine('Example:')
+        writeLine('  $ sign \'{"type":1,"data":{"target":"bob"}}\' -F key.json -M alice')
+    })
+
+program.command('transfer <receiver> <amount>')
+    .alias('xfer')
+    .description('transfer coins')
+    .action(function(receiver, amount) {
+        verifyKeyAndUser()
+        sendTx(cmds.transfer(program.key, program.me, receiver, amount))
+    }).on('--help', function(){
+        writeLine('')
+        writeLine('Example:')
+        writeLine('  $ transfer bob 777 -F key.json -M alice')
+    })
+
+program.command('transfer-bw <receiver> <amount>')
+    .alias('xfer-bw')
+    .description('transfer bandwidth')
+    .action(function(receiver, amount) {
+        verifyKeyAndUser()
+        sendTx(cmds.transferBw(program.key, program.me, receiver, amount))
+    }).on('--help', function(){
+        writeLine('')
+        writeLine('Example:')
+        writeLine('  $ xfer-bw dan 777 -F key.json -M alice')
+    })
+
+program.command('transfer-vt <receiver> <amount>')
+    .alias('xfer-vt')
+    .description('transfer voting tokens')
+    .action(function(receiver, amount) {
+        verifyKeyAndUser()
+        sendTx(cmds.transferVt(program.key, program.me, receiver, amount))
+    }).on('--help', function(){
+        writeLine('')
+        writeLine('Example:')
+        writeLine('  $ xfer-vt charlotte 777 -F key.json -M alice')
+    })
+
+program.command('unfollow <target>')
+    .alias('unsubscribe')
+    .description('stop following another user ')
+    .action(function(target) {
+        verifyKeyAndUser()
+        sendTx(cmds.unfollow(program.key, program.me, target))
+    }).on('--help', function(){
+        writeLine('')
+        writeLine('Example:')
+        writeLine('  $ unfollow bob -F key.json -M alice')
+    })
+
+program.command('unvote-leader <leader>')
+    .description('remove a leader vote')
+    .action(function(leader) {
+        verifyKeyAndUser()
+        sendTx(cmds.disapproveNode(program.key, program.me, leader))
+    }).on('--help', function(){
+        writeLine('')
+        writeLine('Example:')
+        writeLine('  $ unvote-leader bob -F key.json -M alice')
+    })
+
+program.command('vote <link> <author> <vt> <tag>')
+    .description('vote for a content')
+    .action(function(link, author, vt, tag) {
+        verifyKeyAndUser()
+        sendTx(cmds.vote(program.key, program.me, link, author, vt, tag))
+    }).on('--help', function(){
+        writeLine('')
+        writeLine('Arguments:')
+        writeLine('  <link>: the identifier of the comment to vote on')        
+        writeLine('  <author>: the author of the comment to vote on')
+        writeLine('  <vt>: the amount of VT to spend on the vote')
+        writeLine('  <tag>: the tag to associate with the vote')
+        writeLine('')
+        writeLine('Examples:')
+        writeLine('  $ vote root-comment alice 1000 introduce-yourself -F key.json -M bob')
+    })
+
+program.command('vote-leader <leader>')
+    .description('vote for a leader')
+    .action(function(leader) {
+        verifyKeyAndUser()
+        sendTx(cmds.approveNode(program.key, program.me, leader))
+    }).on('--help', function(){
+        writeLine('')
+        writeLine('Example:')
+        writeLine('  $ vote-leader bob -F key.json -M alice')
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // error on unknown commands
 program.on('command:*', function () {
     writeLine('Unknown command: '+program.args[0])
     writeLine('See --help for a list of available commands.')
