308437af7ecf0c0ee74c37dbbbc2564cfed58a10
Adrien M <adrien@d.tube>
2019-04-24 03:37:58 +0200

    v24

--- src/config.js
+++ src/config.js
@@ -3,10 +3,10 @@ var config = {
         0: {
             // the account pricing options
             // see: https://www.wolframalpha.com/input/?i=plot+10%2B100*(1.1%5E(14-x))+from+x%3D1+to+x%3D40
-            accountPriceBase: 0,
-            accountPriceCharMult: 1.1,
-            accountPriceChars: 1,
-            accountPriceMin: 0,
+            accountPriceBase: 100,
+            accountPriceCharMult: 1.3,
+            accountPriceChars: 8,
+            accountPriceMin: 1,
             accountMaxLength: 50,
             accountMinLength: 1,
             // allowed username chars
@@ -21,7 +21,7 @@ var config = {
             // the block time in ms
             blockTime: 3000,
             // the number of ms needed for 1 DTC to generate 1 byte of bw
-            bwGrowth: 1,
+            bwGrowth: 300000,
             // the maximum bandwidth an account can have available
             bwMax: 102400,
             // the number of rounds of consensus before block is valid (min 2)
@@ -55,7 +55,7 @@ var config = {
             // if false master can create accounts with usernames without burning tokens
             masterPaysForUsernames: false,
             // the master account public original key
-            masterPub: 'yLuiXbuU1Pw8SbzJ3BYhcXdCQ81jFPsYwryKEibGWRNo',
+            masterPub: 'ngruo84YEunp8JjJv2xqA2GBCRPuF7nqWkccJ27X6je8',
             // the maximum time drift in ms before a transaction is invalid
             maxDrift: 200,
             // the max length of a transfer memo
@@ -67,7 +67,7 @@ var config = {
             // the maximum number of mentions triggering a notification
             notifMaxMentions: 10,
             // the fake hash of block 0 (new origin hash -> new chain)
-            originHash: '0000000000000000000000000000000000000000000000000000000000000023',
+            originHash: '0000000000000000000000000000000000000000000000000000000000000024',
             randomBytesLength: 32,
             // the minimum hourly reward pool (including leader rewards)
             rewardPoolMin: 10,
@@ -83,6 +83,7 @@ var config = {
             // key: transaction id (see transaction.js:TransactionType)
             // value: null/0 (default): enabled, 1: disabled, 2: master-only
             txLimits: {
+                0: 2,
                 14: 2,
                 15: 2
             },
