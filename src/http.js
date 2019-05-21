ed6a8874b2ccc6bd5a557e273314248a06087f22
Adrien M <adrien@d.tube>
2019-05-22 01:55:17 +0200

    filter out non-matching txs in account history

--- src/http.js
+++ src/http.js
@@ -338,6 +338,17 @@ var http = {
                 query['$and'].push({_id: {$lt: lastBlock}})
             
             db.collection('blocks').find(query, {sort: {_id: -1}, limit: 50}).toArray(function(err, blocks) {
+                for (let b = 0; b < blocks.length; b++) {
+                    var newTxs = []
+                    for (let t = 0; t < blocks[b].txs.length; t++)
+                        if (blocks[b].txs[t].sender === author
+                        || blocks[b].txs[t].data.target === author
+                        || blocks[b].txs[t].data.receiver === author
+                        || blocks[b].txs[t].data.pa === author
+                        || blocks[b].txs[t].data.author === author)
+                            newTxs.push(blocks[b].txs[t])
+                    blocks[b].txs = newTxs
+                }
                 res.send(blocks)
             })
         })
