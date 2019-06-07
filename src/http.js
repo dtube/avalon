3a40dbdb314f7aa8f66d4f3613f301883814f508
Adrien M <adrien@d.tube>
2019-06-07 15:46:52 +0200

    add /distributed/:name endpoint

--- src/http.js
+++ src/http.js
@@ -437,6 +437,18 @@ var http = {
             })
         })
 
+        // get distributions for a user
+        app.get('/distributed/:name', (req, res) => {
+            if (!req.params.name) {
+                res.sendStatus(500)
+                return
+            }
+            db.collection('distributed').find({name: req.params.name}, {sort: {ts: -1}, limit: 200}).toArray(function(err, distributions) {
+                if (!distributions) res.sendStatus(404)
+                else res.send(distributions)
+            })
+        })
+
         // test api (should be separated)
         // get youtube info
         app.get('/youtube/:videoId', (req, res) => {
