c58b558725393012d696b959b886574199dda14e
Adrien M <adrien@d.tube>
2019-07-06 00:24:53 +0200

    add proper 404 for /block

--- src/http.js
+++ src/http.js
@@ -19,6 +19,10 @@ var http = {
             var blockNumber = parseInt(req.params.number)
             db.collection('blocks').findOne({_id: blockNumber}, function(err, block) {
                 if (err) throw err
+                if (!block) {
+                    res.sendStatus(404)
+                    return
+                }
                 res.send(block)
             })
         })
