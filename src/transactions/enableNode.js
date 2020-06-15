ad548a42c741fd81c67131863b8007389d6fc6a3
Adrien M <adrien@d.tube>
2020-06-16 01:19:47 +0200

    correctly unset leader key instead of empty string

--- src/transactions/enableNode.js
+++ src/transactions/enableNode.js
@@ -5,17 +5,22 @@ module.exports = {
         cb(true)
     },
     execute: (tx, ts, cb) => {
-        // because if key is incorrect, we just null it
-        var pub_leader = null
+        // because if key is incorrect, we just unset it
         if (validate.publicKey(tx.data.pub, config.accountMaxLength))
-            pub_leader = tx.data.pub
-
-        cache.updateOne('accounts', {
-            name: tx.sender
-        },{ $set: {
-            pub_leader: pub_leader
-        }}, function(){
-            cb(true)
-        })
+            cache.updateOne('accounts', {
+                name: tx.sender
+            },{ $set: {
+                pub_leader: tx.data.pub
+            }}, function(){
+                cb(true)
+            })
+        else
+            cache.updateOne('accounts', {
+                name: tx.sender
+            },{ $unset: {
+                pub_leader: ''
+            }}, function(){
+                cb(true)
+            })
     }
 }
