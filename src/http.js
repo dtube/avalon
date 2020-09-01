a79c80365103fbc41d7ca18a108758f4b67243e0
techcoderx <engtianxi@outlook.com>
2020-09-01 14:25:39 +0800

    fix missing removeListener callback

--- src/http.js
+++ src/http.js
@@ -98,8 +98,9 @@ var http = {
                     transaction.addToPool([tx])
 
                     var transactTimeout = setTimeout(function() {
-                        transaction.eventConfirmation.removeListener(tx.hash)
-                        res.status(408).send({error: 'transaction timeout'})
+                        transaction.eventConfirmation.removeListener(tx.hash,() => {
+                            res.status(408).send({error: 'transaction timeout'})
+                        })
                     }, timeout_transact_async)
 
                     transaction.eventConfirmation.addListener(tx.hash, function() {
