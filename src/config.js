558a3f5ebb9fe9c4cd9224da2a1bd34f23d32fef
Adrien M <adrien@d.tube>
2020-09-18 20:28:54 +0200

    change accounts prices

--- src/config.js
+++ src/config.js
@@ -1,13 +1,12 @@
 var config = {
     history: {
         0: {
-            // this is configuration for testnet2
-            // the account pricing options
-            // see: https://www.wolframalpha.com/input/?i=plot+10%2B100*(1.1%5E(14-x))+from+x%3D1+to+x%3D40
-            accountPriceBase: 10000,
-            accountPriceCharMult: 1.3,
-            accountPriceChars: 8,
-            accountPriceMin: 1,
+            // this is configuration for mainnet
+            // wip
+            accountPriceBase: 20000,
+            accountPriceCharMult: 4,
+            accountPriceChars: 5,
+            accountPriceMin: 200,
             accountMaxLength: 50,
             accountMinLength: 1,
             // allowed username chars
@@ -36,7 +35,7 @@ var config = {
             // the required number of ms before a vote reward can be claimed
             ecoClaimTime: 604800000, // 7 days
             // can the first vote rewards be altered by downvotes like other votes
-            ecoPunishAuthor: false,
+            ecoPunishAuthor: true,
             // the percentage of coins that are burnt when there are opposite votes with rewards
             ecoPunishPercent: 0.5,
             // the number of ms before a vote reaches 100% rentability
@@ -91,7 +90,7 @@ var config = {
             // the maximum number of mentions triggering a notification
             notifMaxMentions: 10,
             // the sha256sum hash of block 0 (new origin hash -> new chain)
-            originHash: '8cca1ec6d023d0182cc1501ef62af9ba9b0e20499e218cf400805b6d8764182d',
+            originHash: '1100000000000000000000000000000000000000000000000000000000000000',
             // the default number of random bytes to use for new key generation
             randomBytesLength: 32,
             // the minimum hourly reward pool (including leader rewards)
@@ -105,6 +104,7 @@ var config = {
             // the maximum length of tags (on votes)
             tagMaxLength: 25,
             tagMaxPerContent: 5,
+            tmpForceTs: true,
             // the time after which transactions expire and wont be accepted by nodes anymore
             txExpirationTime: 60000,
             // limit which transactions are available
@@ -118,12 +118,6 @@ var config = {
             // the number of ms needed for 0.01 DTC to generate 1 vt
             vtGrowth: 360000000, // +1 vt per hour per DTC (3600 * 1000 * 100)
             vtPerBurn: 6 // can be updated in the future to modify incentives
-        },
-        350000: {
-            ecoPunishAuthor: true
-        },
-        2050000: {
-            tmpForceTs: true
         }
     },
     read: (blockNum) => {
