78269190c22c557d6941565a923b023eca86cb8a
Adrien M <adrien@d.tube>
2020-09-30 17:17:47 +0200

    change start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,11 +5,11 @@
 #export P2P_PORT=6001
 
 # MongoDB configuration
-export DB_NAME=avalon2
+#export DB_NAME=avalon
 #export DB_URL=mongodb://localhost:27017
 
 # Peering configuration
-export OFFLINE=1
+#export OFFLINE=1
 #export NO_DISCOVERY=1
 #export DISCOVERY_EXCLUDE=dtube
 
@@ -26,15 +26,15 @@ export WARN_SLOW_VALID=5
 export WARN_SLOW_EXEC=5
 
 # trace / perf / econ / cons / debug / info / warn
-export LOG_LEVEL=debug
+export LOG_LEVEL=info
 
 # groups blocks during replay output to lower screen spam
-export REPLAY_OUTPUT=1
+export REPLAY_OUTPUT=100
 
 # Rebuild chain state from dump, verifying every block and transactions
 # Do not forget to comment this out after rebuild
 #export REBUILD_STATE=1
-#export REBUILD_RESUME_BLK=1268031
+#export REBUILD_RESUME_BLK=
 
 # default peers to connect with on startup
 export PEERS=
