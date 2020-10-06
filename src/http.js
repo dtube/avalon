4f669390f27a0d7d093b6e2f2c2060d3afa77a17
Adrien M <adrien@d.tube>
2020-10-06 21:40:28 +0200

    add /rewards api endpoint

--- src/http.js
+++ src/http.js
@@ -69,6 +69,60 @@ var http = {
             })
         })
 
+        // pending reward per user
+        app.get('/rewards/pending/:name', (req,res) => {
+            if (!req.params.name) {
+                res.sendStatus(500)
+                return
+            }
+            db.collection('contents').aggregate([
+                {$unwind: "$votes"},
+                {$match:{
+                    "votes.u": req.params.name,
+                    "votes.claimed": {$exists:false}
+                }},{$group:{_id: 0, total: {$sum: "$votes.claimable"}}}
+            ]).toArray(function(err,result) {
+                if (err) {
+                    console.log(err)
+                    res.sendStatus(500)
+                    return
+                } else {
+                    if (!result[0] || !result[0].total) {
+                        res.send({total: 0})
+                        return
+                    }
+                    res.send({total: Math.round(1000*result[0].total)/1000})
+                }
+            })
+        })
+
+        // claimed reward per user
+        app.get('/rewards/claimed/:name', (req,res) => {
+            if (!req.params.name) {
+                res.sendStatus(500)
+                return
+            }
+            db.collection('contents').aggregate([
+                {$unwind: "$votes"},
+                {$match:{
+                    "votes.u": req.params.name,
+                    "votes.claimed": {$exists:true}
+                }},{$group:{_id: 0, total: {$sum: "$votes.claimable"}}}
+            ]).toArray(function(err,result) {
+                if (err) {
+                    console.log(err)
+                    res.sendStatus(500)
+                    return
+                } else {
+                    if (!result[0] || !result[0].total) {
+                        res.send({total: 0})
+                        return
+                    }
+                    res.send({total: Math.round(1000*result[0].total)/1000})
+                }
+            })
+        })
+
         // generate a new key pair
         app.get('/newKeyPair', (req, res) => {
             res.send(chain.getNewKeyPair())
