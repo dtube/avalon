86c6c250abc4233f6962d1f60ff3641de295fc59
Adrien M <adrien@d.tube>
2020-10-26 01:46:53 +0100

    add back old /votes/:voter call

--- src/http.js
+++ src/http.js
@@ -491,6 +491,40 @@ var http = {
                 res.send(votes)
             })
         })
+        // adding back this one for compatibility purpose
+        // to remove in future
+        app.get('/votes/:voter/:lastTs', (req, res) => {
+            var voter = req.params.voter
+            var query = {
+                $and: [{
+                    'votes.u': voter,
+                }]
+            }
+            var lastTs = parseInt(req.params.lastTs)
+            if (lastTs > 0)
+                query['$and'].push({ ts: { $lt: lastTs } })
+
+            db.collection('contents').find(query, { sort: { ts: -1 }, limit: 50 }).toArray(function (err, contents) {
+                if (err) throw err
+                var votes = []
+                for (let i = 0; i < contents.length; i++) {
+                    for (let y = 0; y < contents[i].votes.length; y++) {
+                        if (contents[i].votes[y].u === voter)
+                            votes.push({
+                                author: contents[i].author,
+                                link: contents[i].link,
+                                claimable: contents[i].votes[y].claimable,
+                                claimed: contents[i].votes[y].claimed,
+                                vt: contents[i].votes[y].vt,
+                                ts: contents[i].votes[y].ts,
+                                contentTs: contents[i].ts,
+                                burn: contents[i].votes[y].burn
+                            })
+                    }
+                }
+                res.send(votes)
+            })
+        })
 
         // get pending votes history of a user
         app.get('/votes/pending/:voter/:lastTs', (req, res) => {
