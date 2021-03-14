8188159a23880fd397173e4edc6abc212261d2f9
techcoderx <engtianxi@outlook.com>
2021-03-14 14:57:10 +0800

    improve /count query

--- src/http/count/index.js
+++ src/http/count/index.js
@@ -3,11 +3,14 @@ module.exports = {
         // count how many blocks are in the node
         // todo: kill this endpoint and replace by /block/latest (faster execution and same result)
         app.get('/count', (req, res) => {
-            db.collection('blocks').countDocuments(function (err, count) {
-                if (err) throw err
-                res.send({
-                    count: count
-                })
+            db.collection('blocks').findOne({},{ 
+                sort: { _id: -1 },
+                projection: { _id: 1 }
+            },(err, count) => {
+                if (err)
+                    res.status(500).send({error: 'could not get latest block'})
+                else
+                    res.send({ count: count._id })
             })
         })
     }
