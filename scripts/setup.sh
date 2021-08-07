1cd755b4f96850e54433c337ebf1a7058b9f5a14
Jared Thorne <dev.deckardcain@gmail.com>
2021-08-07 18:59:24 -0400

    A setup script for those who need their databases setup

---
+++ scripts/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir genesis
+cd genesis
+wget https://backup.d.tube/genesis.zip
+cd ..
+mkdir dump
+cd dump
+wget https://backup.d.tube/$(date +%H).tar.gz
+tar xfvz ./*
+mongo avalon --eval "printjson(db.dropDatabase())"
+mongorestore -d avalon ./
+cd ..
+rm -rf ./dump
+echo "DONE! You're now free to run: ./scripts/start.sh"
