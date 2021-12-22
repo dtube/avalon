eff714f6bb333a174896d0d8e4e57f7eeb32f690
techcoderx <engtianxi@outlook.com>
2021-12-22 21:38:04 +0800

    account history api v2

--- src/http/history/index.js
+++ src/http/history/index.js
@@ -44,5 +44,41 @@ module.exports = {
                 res.send(blocks)
             })
         })
+
+        // history v2, eventually this should replace the above
+        app.get('/history2/:author/:lastBlock/:skip?', (req, res) => {
+            let lastBlock = parseInt(req.params.lastBlock)
+            let skip = parseInt(req.params.skip)
+            let author = req.params.author
+            let query = {
+                $and: [
+                    { $or: [
+                        {'sender': author},
+                        {'data.target': author},
+                        {'data.receiver': author},
+                        {'data.pa': author},
+                        {'data.author': author}
+                    ]}
+                ]
+            }
+            let filter = {
+                sort: {includedInBlock: -1},
+                limit: 50
+            }
+    
+            if (lastBlock > 0) 
+                query['$and'].push({includedInBlock: {$lt: lastBlock}})
+            
+            if (!isNaN(skip) && skip > 0)
+                filter.skip = skip
+    
+            db.collection('txs').find(query, filter).toArray(function(err, txs) {
+                if (err || !txs)
+                    return res.status(500).send({error: 'failed to query account history'})
+                for (let t = 0; t < txs.length; t++)
+                    delete txs[t]._id
+                res.send(txs)
+            })
+        })
     }
 }
