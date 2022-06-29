153837c29c60a489e47f15b776856784856010a5
techcoderx <engtianxi@outlook.com>
2022-06-29 12:02:07 +0800

    include leader stats in /allminers api

--- src/http/allminers/index.js
+++ src/http/allminers/index.js
@@ -11,14 +11,26 @@ module.exports = {
             db.collection('accounts').find({ node_appr: { $gt: 0 } }, {
                 sort: { node_appr: -1 }
             }).toArray(function (err, accounts) {
-                if (err) throw err
-                for (let i in accounts) {
-                    accounts[i].followsCount = (accounts[i].follows ? accounts[i].follows.length : 0)
-                    accounts[i].followersCount = (accounts[i].followers ? accounts[i].followers.length : 0)
-                    delete accounts[i].follows
-                    delete accounts[i].followers
-                }
-                res.send(accounts)
+                if (err) return res.status(500).send({ error: 'failed to fetch miners' })
+                db.collection('leaders').find({ voters: { $gt: 0 } },{}).toArray((err,leaders) => {
+                    if (err) return res.status(500).send({ error: 'failed to fetch leader stats' })
+                    let leaderObj = {}
+                    for (let i in leaders)
+                        leaderObj[leaders[i]._id] = leaders[i]
+                    for (let i in accounts) {
+                        accounts[i].subbed = (accounts[i].follows ? accounts[i].follows.length : 0)
+                        accounts[i].subs = (accounts[i].followers ? accounts[i].followers.length : 0)
+                        delete accounts[i].follows
+                        delete accounts[i].followers
+                        delete accounts[i].keys
+                        delete accounts[i].auths
+                        if (leaderObj[accounts[i].name]) {
+                            accounts[i].leaderStat = leaderObj[accounts[i].name]
+                            delete accounts[i].leaderStat._id
+                        }
+                    }
+                    res.send(accounts)
+                })
             })
         })
     }
